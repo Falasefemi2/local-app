6205655709a5a4ae44c4edeec39f3b7370292b3c
falase femi <femifalase228@gmail.com>
2026-05-04 14:49:38 +0100

    database table done

---
+++ src/db/daily-log.ts
@@ -0,0 +1,25 @@
+import { DateTime, Schema } from "effect";
+import { PrimaryKeyIndex } from "./shared";
+
+export class DailyLogInsert extends Schema.Class<DailyLogInsert>(
+  "DailyLogInsert",
+)({
+  date: Schema.DateTimeUtc,
+  planId: PrimaryKeyIndex,
+}) {}
+
+export class DailyLogSelect extends Schema.Class<DailyLogSelect>(
+  "DailyLogSelect",
+)({
+  date: Schema.DateTimeUtc,
+  planId: PrimaryKeyIndex,
+}) {
+  static readonly formatDate = DateTime.formatIsoDateUtc;
+}
+
+export class DailyLogUpdate extends Schema.Class<DailyLogUpdate>(
+  "DailyLogUpdate",
+)({
+  date: Schema.DateTimeUtcFromSelf,
+  planId: PrimaryKeyIndex,
+}) {}
